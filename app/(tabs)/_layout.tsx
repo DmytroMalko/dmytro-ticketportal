dc470567b0551a14f84553afdea32333c4d7f478
dima <11malko.dima@gmail.com>
2025-07-30 16:41:58 +0200

    Cleanup, removing unused files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-import AntDesign from '@expo/vector-icons/AntDesign';
-
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -14,37 +12,43 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, size }) => <FontAwesome name="home" color={color} size={size}/>,
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="teams"
         options={{
-          title: 'Teams',
-          tabBarIcon: ({ color, size }) => <AntDesign name="team" color={color} size={size}/>,
+          title: "Teams",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="team" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="matches"
         options={{
-          title: 'Matches',
-          tabBarIcon: ({ color, size }) => <FontAwesome name="soccer-ball-o" color={color} size={size}/>,
+          title: "Matches",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="soccer-ball-o" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="about"
         options={{
-          title: 'About',
-          tabBarIcon: ({ color, size }) => <AntDesign name="infocirlceo" color={color} size={size}/>,
+          title: "About",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="infocirlceo" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
