96ec699a77a558efbb5a45181798219210c18eef
dima <11malko.dima@gmail.com>
2025-07-30 10:53:41 +0200

    Basic navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Tabs } from 'expo-router';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -29,29 +22,29 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => <FontAwesome name="home" color={color} size={size}/>,
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="teams"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Teams',
+          tabBarIcon: ({ color, size }) => <AntDesign name="team" color={color} size={size}/>,
+        }}
+      />
+      <Tabs.Screen
+        name="matches"
+        options={{
+          title: 'Matches',
+          tabBarIcon: ({ color, size }) => <FontAwesome name="soccer-ball-o" color={color} size={size}/>,
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, size }) => <AntDesign name="infocirlceo" color={color} size={size}/>,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,53 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useEffect, useRef, useState } from 'react';
 
-export default function TabOneScreen() {
+
+interface QuoteObject {
+  quote: string;
+  author: string;
+  id: string;
+}
+
+
+export default function HomeScreen() {
+  const [quoteObject, setQuoteObject] = useState<QuoteObject | null>(null)
+
+  // why used this? because since react 18 in strict mode it lauch useEffect with empty dependecy twice, it shall not be happening at product but in development it throw sometimes errors on dupicated run or data misleads
+  const hasFetched = useRef(false);
+  
+  useEffect(() => {
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/quotes/random');
+        const data = await response.json() as QuoteObject;
+        
+        if (data && data.quote) {
+          setQuoteObject(data)
+        }
+
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Vítejte zpět!</Text>
+      <Text>Citát dnešního dne je:</Text>
+      {quoteObject && quoteObject?.quote.length > 0 && (
+        <View style={styles.box} >
+          <Text>{quoteObject?.quote}</Text>
+          <Text style={styles.author}>Autor: {quoteObject?.author}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -23,9 +63,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  box: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  author: {
+    fontStyle: 'italic'
+  }
 });
