814b4f60a9018cb84a3820d16ec79069ca3355bc
dima <11malko.dima@gmail.com>
2025-07-30 16:37:09 +0200

    About

--- components/EditMatchForm.tsx
+++ components/EditMatchForm.tsx
@@ -14,6 +14,7 @@ interface FormData {
 }
 
 const EditMatchForm: React.FC<EditMatchFormProps> = ({ match }) => {
+  //Rgex validaion could be added for score
   const {
     control,
     handleSubmit,
